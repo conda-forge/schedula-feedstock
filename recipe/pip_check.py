75c78e188fc2d09d52db9d5ce2c63dfa15ffa65c
Nicholas Bollweg <nick.bollweg@gmail.com>
2024-10-30 16:10:46 -0500

    ignore multiprocess pip check error

---
+++ recipe/pip_check.py
@@ -0,0 +1,37 @@
+"""`pip check` with regexes to ignore lines.
+
+Usage:
+
+    python pip_check.py [ignore_regex...]
+
+Example:
+
+    python pip_check.py "multiprocess .* is not supported on this platform"
+
+"""
+import sys
+import subprocess
+import re
+
+def main(*ignores: str) -> int:
+    proc = subprocess.Popen([sys.executable, "-m", "pip", "check"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding="utf-8")
+    proc.communicate()
+
+    assert proc.stdout
+    raw = proc.stdout.read().strip()
+
+    errors = []
+    for line in raw.splitlines():
+        if not line.strip():
+            continue
+        elif any(re.findall(i, line) for i in ignores):
+            print("... ignoring:", line)
+        else:
+            errors += [line]
+
+    print("\n".join(errors))
+
+    return len(errors)
+
+if __name__ == "__main__":
+    sys.exit(main("No broken requirements found", *sys.argv[1:]))
